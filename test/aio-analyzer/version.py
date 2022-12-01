3036ebd167913014227ae0d81b9336483bd3501b
qth2hi <holger.queckenstedt@de.bosch.com>
2022-12-01 10:30:21 +0100

    aio-analyzer adapted to renamed packages

--- test/aio-analyzer/version.py
+++ test/aio-analyzer/version.py
@@ -19,6 +19,6 @@
 # Version and date of aio-analyzer
 #
 NAME         = "RobotFramework AIO Analyzer"
-VERSION      = "0.1.0"
-VERSION_DATE = "28.09.2022"
+VERSION      = "0.2.0"
+VERSION_DATE = "01.12.2022"
 
