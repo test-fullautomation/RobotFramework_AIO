4f0fefa17a9e602ab4a0e4082dc3dd724755e60f
qth2hi <holger.queckenstedt@de.bosch.com>
2022-12-06 11:48:22 +0100

    Replaced usage of executerobottest.py by executepytest.py (because of planned rework of the JsonPreprocessor atest)

--- test/aio-test-trigger/version.py
+++ test/aio-test-trigger/version.py
@@ -19,6 +19,6 @@
 # Version and date of aio-test-trigger
 #
 NAME         = "RobotFramework AIO Test Trigger"
-VERSION      = "0.10.0"
-VERSION_DATE = "05.12.2022"
+VERSION      = "0.11.0"
+VERSION_DATE = "06.12.2022"
 
