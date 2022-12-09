478a4b5224dc82ecdde7bd04339cfda016f33e90
Tran Hoang Nguyen <agt2hc@bosch.com>
2022-12-09 13:53:29 +0700

    removed dlt install script because this is OSS version

--- config/build/dpkg_build/install_dlt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DLTCONNECTOR_PATH="/opt/bosch/robfw/python39/install/lib/python3.9/site-packages/QConnectionDLTLibrary/tools/DLTConnector/linux/"
-
-sudo dpkg -r DLTConnector
-unalias DLTConnector
-sudo dpkg -i "${DLTCONNECTOR_PATH}DLTConnector_v1.3.9.deb"
-
-crontab -l | grep -v '@reboot /home/ugc1hc/Project/crontest/install_dlt.sh'  | crontab -
