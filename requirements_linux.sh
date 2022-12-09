4546c1d2c0541adee5620a679d42bb8b63836508
ntd1hc <ngoan.tranduy@vn.bosch.com>
2022-12-09 15:13:12 +0700

    separate OS requirements for runners as requirements_* files

---
+++ requirements_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get install -y libxshmfence1 libglu1
+sudo apt-get install -y libatk1.0-0
+sudo apt-get install -y libatk-bridge2.0-0
+sudo apt-get install -y libgdk-pixbuf2.0-dev
+sudo apt-get install -y libgtk-3-0
+sudo apt-get install -y libgbm-dev
+sudo apt-get install -y libasound2-dev
+sudo apt-get install -y zstd
+sudo apt-get install -y xmlstarlet
+sudo apt-get install -y curl
+sudo apt-get install -y default-jre
+sudo apt-get install -y libmariadb-dev-compat libmariadb-dev
+sudo apt-get install -y clang
+sudo apt-get install -y unixodbc-dev
+sudo apt-get install -y python3-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y pandoc
+sudo apt-get install -y texlive-latex-*
