01d2f94bb053b13542a1fad56136aeec9754f9ea
ntd1hc <ngoan.tranduy@vn.bosch.com>
2023-03-17 13:14:29 +0700

    restore all jobs in build pipeline

--- requirements_windows.sh
+++ requirements_windows.sh
@@ -30,7 +30,13 @@ texlive_packages=(
 #   extra_packages+="$package,"
 # done
 
-TEXLIVE_DIR="C:/texlive/aio"
+if [ -n $GENDOC_LATEXPATH ]; then
+  TEXLIVE_DIR=${GENDOC_LATEXPATH%/bin/win32}
+else
+  TEXLIVE_DIR="C:/texlive/aio"
+  export GENDOC_LATEXPATH="${TEXLIVE_DIR}/bin/win32"
+fi
+
 # choco install texlive --version=2022.20221202 --params "'/collections:pictures,latex,latexextra,latexrecommended'" --execution-timeout 5400
 choco install texlive --params "'/collections:pictures,latex /InstallationPath:${TEXLIVE_DIR}'"
 
@@ -43,5 +49,3 @@ $tlmgr update --self --all
 for package in ${texlive_packages[@]}; do
   $tlmgr install "$package"
 done
-
-export GENDOC_LATEXPATH="${TEXLIVE_DIR}/bin/win32"
