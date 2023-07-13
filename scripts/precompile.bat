c7bfc290e003e1acc2f37e23881cbb16251825e4
ntd1hc <ngoan.tranduy@vn.bosch.com>
2023-07-13 10:58:31 +0700

    Windows: change letter of virtual drive from A to R

--- scripts/precompile.bat
+++ scripts/precompile.bat
@@ -1,7 +1,7 @@
 @echo off
 REM NB: Removing any previous association to be sure new one will work
-subst A: /D 1> NUL 2>&1
-subst A: "%~dp0..\.."
+subst R: /D 1> NUL 2>&1
+subst R: "%~dp0..\.."
 
 if "%1"=="" (
     powershell -File "%~dp0\PowerShell\create_project_config.ps1"
