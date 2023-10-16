5c1178e47dfafde132088db645999f0c33d6f460
ntd1hc <ngoan.tranduy@vn.bosch.com>
2023-10-16 17:16:34 +0700

    update script to check JDK_PATH to avoid empty value for JAVA_HOME

--- config/build/dpkg_build/set_robotenv.sh
+++ config/build/dpkg_build/set_robotenv.sh
@@ -13,5 +13,8 @@ export GENDOC_PLANTUML_PATH=$RobotVsCode/data/extensions/jebbs.plantuml-2.17.5
 
 # Check if JAVA_HOME is already set
 if [ -z "$JAVA_HOME" ]; then
-    export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java)))) 2> /dev/null
+    JDK_PATH=$(readlink -f $(which java) 2> /dev/null) 2> /dev/null
+    if [ -n "$JDK_PATH" ]; then
+        export JAVA_HOME=$(dirname $(dirname $JDK_PATH)) 2> /dev/null
+    fi
 fi
