66dbc3511828aa36629f0a7bd315bc09d54acf6b
mar3hc <tri.maiminh@vn.bosch.com>
2025-04-14 18:03:30 +0700

    add step to download avd packages

---
+++ config/tools/download_avd_packages.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+########################################################################################
+
+#setlocal enabledelayedexpansion
+mypath=$(realpath $(dirname $0))
+destDir=$(realpath "$ANDROID_HOME")
+
+UNAME=$(uname)
+if [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+	mkdir -p "$ANDROID_HOME"/aehd-windows
+	mkdir -p "$ANDROID_HOME"/system-images/android-34/google_apis
+else
+	errormsg "Operation system '$UNAME' is not supported."
+fi
+
+aehdpath=$(realpath "$ANDROID_HOME"/aehd-windows)
+apispath=$(realpath "$ANDROID_HOME"/system-images/android-34/google_apis)
+
+use_cntlm="No"
+
+#
+# import common bash scripts
+#
+. $mypath/../../include/bash/common.sh
+
+function restart_cntlm(){
+	if [ "$UNAME" == "Linux" ] ; then
+		sudo systemctl restart cntlm
+	elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+		net stop cntlm
+		sleep 3
+		net start cntlm
+	fi
+	sleep 1
+}
+#
+#  download packages function
+#
+####################################################
+
+function download_package(){
+	proxy_args=""
+	if [ "$use_cntlm" == "Yes" ]; then
+		proxy_args="--proxy-ntlm -x 127.0.0.1:3128"
+	fi
+	package_name=$1
+	package_url=$2
+	package_out=$3
+
+	retry_counter=0
+	max_retries=5
+	success=false
+
+	echo curl $proxy_args "$package_url" -o "$package_out"
+	while [[ "$retry_counter" -lt "$max_retries" && "$success" == "false" ]];do
+		curl $proxy_args -L -k "$package_url" -o "$package_out"
+
+		if [ $? -eq 0 ]; then
+			success=true
+			goodmsg "Successfully downloaded $package_name"
+		else
+			((retry_counter++))
+			echo "Failed to download $package_url (attempt: $retry_counter)"
+			sleep 1
+			if [ "$use_cntlm" == "Yes" ]; then
+				restart_cntlm
+			fi
+		fi
+	done
+
+	if [ "$success" == "false" ]; then
+		errormsg "FATAL: Could not download $package_url after $max_retries attempts"
+	fi
+}
+
+function packaging_android() {
+	download_android_emulator_hypervisor_driver=https://github.com/google/android-emulator-hypervisor-driver/releases/download/v2.2/aehd-windows_v2_2_0.zip
+	download_android_google_apis=https://dl.google.com/android/repository/sys-img/google_apis/x86_64-34_r13.zip
+
+	archived_android_emulator_hypervisor_driver=aehd-windows_v2_2_0.zip
+	archived_android_google_apis=x86_64-34_r13.zip
+
+	echo "Packaging Android ..."
+
+	npm_proxy_args=""
+	if [ "$use_cntlm" == "Yes" ]; then
+		npm_proxy_args="--proxy=http://localhost:3128"
+	fi
+
+	if [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+		echo "Downloading Android Emulator hypervisor driver"
+		download_package "AEHD" "${download_android_emulator_hypervisor_driver}" "${destDir}/${archived_android_emulator_hypervisor_driver}"
+		/usr/bin/yes A | unzip "${destDir}/${archived_android_emulator_hypervisor_driver}" -d "${aehdpath}"
+		rm -rf "$destDir/${archived_android_emulator_hypervisor_driver}"
+	fi
+
+	echo "Downloading Android Google APIs"
+	download_package "Android Google APIs" "${download_android_google_apis}" "${destDir}/${archived_android_google_apis}"
+	if [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+		/usr/bin/yes A | unzip "${destDir}/${archived_android_google_apis}" -d "${apispath}"
+		rm -rf "$destDir/${archived_android_google_apis}"
+	fi
+}
+
+function make_android() {
+	packaging_android
+}
+
+make_android
