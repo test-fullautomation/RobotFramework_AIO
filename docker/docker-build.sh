8e205886644dd480e535fd9bc3774085b63becfa
Mai Minh Tri (MS/EMC51-XC) <Tri.MaiMinh@vn.bosch.com>
2026-04-17 13:32:44 +0700

    optimize image size

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo ">>>> [1/5] SET DYNAMIC VERSIONING ENV..."
+echo ">>>> [1/7] SET DYNAMIC VERSIONING ENV"
 export AIO_VERSION_DATE=$(date +%m.%Y)
 export AIO_VERSION=${TAG_NAME#[rd]e[vl]/aio/}
 export RobotFrameworkVersion=${TAG_NAME#[rd]e[vl]/aio/}
@@ -12,12 +12,30 @@ export MAINDOC_CONFIGFILE="--configfile ./maindoc/maindoc_configs/maindoc_config
 export PYBIN=$(which python3)
 export PYTHONBIN=$(which python3)
 
-echo ">>>> [2/5] PREPARE SCRIPTS & CONFIG..."
+echo ">>>> [2/7] PREPARE SCRIPTS & CONFIG"
 sed -i 's|../python3lx|/usr/local|g' build
 sed -i 's|${PYDIR}/bin/python3|/usr/local/bin/python3|g' build
 sed -i 's|/opt/rfwaio/python3|/usr/local|g' ./config/build/dpkg_build/robot
 
-echo ">>>> [3/5] CLONE AND INSTALL COMPONENTS..."
+echo ">>>> [3/7] CLEANING SCRIPTS & PERMISSIONS"
+dos2unix ./build \
+         ./cloneall \
+         ./requirements_linux.sh \
+         ./install/install.sh \
+         ./docker/docker-entrypoint.sh
+
+chmod +x ./requirements_linux.sh \
+         ./cloneall \
+         ./install/install.sh \
+         ./build \
+         ./docker/docker-entrypoint.sh
+
+echo ">>>> [4/7] GLOBAL PYTHON REQUIREMENTS"
+./requirements_linux.sh
+python3 -m pip install --no-cache-dir build
+python3 -m pip install --no-cache-dir -r ./install/python_requirements_lx.txt
+
+echo ">>>> [5/7] BUILD"
 mkdir -p /usr/local/lib/plantuml
 curl -L https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar -o /usr/local/lib/plantuml/plantuml.jar
 mkdir -p /opt/rfwaio/robotvscode/data/extensions/jebbs.plantuml-2.18.1/
@@ -25,17 +43,17 @@ ln -sf /usr/local/lib/plantuml/plantuml.jar /opt/rfwaio/robotvscode/data/extensi
 
 ./cloneall --config-file="$REPO_CONFIG"
 ./install/install.sh --android
-
-echo ">>>> [4/5] EXECUTE BUILD LOGIC..."
 ./build --config-file=$REPO_CONFIG --sub-version=$SubVersion
 
-echo ">>>> [5/5] SYSTEM INSTALLATION & PERMISSIONS..."
+echo ">>>> [6/7] SYSTEM INSTALLATION & PERMISSIONS"
 mkdir -p /opt/rfwaio/python3/bin /opt/rfwaio/tools /opt/rfwaio/linux/icon /opt/rfwaio/tutorial /opt/rfwaio/documentation /opt/rfwaio/devtools
 
 # Python Path Compatibility
 ln -s /usr/local/bin/python3 /opt/rfwaio/python3/bin/python3
 
 # Executables & Assets
+cp ./docker/docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+chmod +x /usr/local/bin/docker-entrypoint.sh
 cp ./config/build/dpkg_build/robot /usr/local/bin/robot
 chmod +x /usr/local/bin/robot
 cp ./config/build/dpkg_build/postinst.sh /usr/local/bin/initRobotFrameworkAIO.sh
@@ -64,5 +82,11 @@ cp -R -a /opt/rfwaio/tutorial/. ~/RobotTest/tutorial
 cp -R -a /opt/rfwaio/documentation/. ~/RobotTest/documentation
 chown -R root:robot-aio ~/RobotTest/
 chmod -R 0775 ~/RobotTest/
+
+echo ">>>> [7/7] CLEANING UP BUILD"
+find /usr/local/lib/python3.13 -type d -name "__pycache__" -exec rm -rf {} +
+find /usr/local/lib/python3.13 -name "*.pyc" -delete
+# Remove Appium Inspector
+rm -f /opt/rfwaio/devtools/Appium-Inspector.AppImage
 
 echo ">>>> Build OSS Finished successfully!"
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,3 +1,10 @@
-/usr/local/bin/initRobotFrameworkAIO.sh
+#!/bin/bash
+# Check if JAVA_HOME is already set
+if [ -z "$JAVA_HOME" ]; then
+    JDK_PATH=$(readlink -f $(which java) 2> /dev/null) 2> /dev/null
+    if [ -n "$JDK_PATH" ]; then
+        export JAVA_HOME=$(dirname $(dirname $JDK_PATH)) 2> /dev/null
+    fi
+fi
 
-/opt/rfwaio/linux/set_robotenv.sh
+exec "$@"
