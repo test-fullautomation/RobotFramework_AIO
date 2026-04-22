53b25357286badcf6b7d46131e2ba96c0c4e1e22
Mai Minh Tri (MS/EMC51-XC) <Tri.MaiMinh@vn.bosch.com>
2026-04-22 13:49:02 +0700

    fix pip installation issues and add BIOS proxy support

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-echo ">>>> [1/7] SET DYNAMIC VERSIONING ENV"
+RF_OPT_DIR="/opt/rfwaio"
+PYTHON_SITE_PACKAGES="$RF_OPT_DIR/python3/lib/python3.13/site-packages"
+CURL_OPTS="-L"
+
+echo ">>>> [1/6] SET DYNAMIC VERSIONING ENV"
 export AIO_VERSION_DATE=$(date +%m.%Y)
 export AIO_VERSION=${TAG_NAME#[rd]e[vl]/aio/}
 export RobotFrameworkVersion=${TAG_NAME#[rd]e[vl]/aio/}
@@ -9,84 +13,99 @@ export BUNDLE_VERSION_DATE="--bundle_version_date $(date +%m.%Y)"
 export BUNDLE_VERSION="--bundle_version ${TAG_NAME#[rd]e[vl]/aio/}"
 export REPO_CONFIG="./config/repositories/repositories_${VERSION_TYPE}.conf"
 export MAINDOC_CONFIGFILE="--configfile ./maindoc/maindoc_configs/maindoc_config_OSS_${VERSION_TYPE}.json"
-export PYBIN=$(which python3)
-export PYTHONBIN=$(which python3)
 
-echo ">>>> [2/7] PREPARE SCRIPTS & CONFIG"
-sed -i 's|../python3lx|/usr/local|g' build
-sed -i 's|${PYDIR}/bin/python3|/usr/local/bin/python3|g' build
-sed -i 's|/opt/rfwaio/python3|/usr/local|g' ./config/build/dpkg_build/robot
+echo ">>>> [2/6] CLEANING SCRIPTS & PERMISSIONS"
+SCRIPTS="./build ./cloneall ./install/install.sh ./install/versions.conf ./docker/docker-entrypoint.sh ./requirements_linux.sh"
+dos2unix $SCRIPTS
+chmod +x $SCRIPTS
 
-echo ">>>> [3/7] CLEANING SCRIPTS & PERMISSIONS"
-dos2unix ./build \
-         ./cloneall \
-         ./requirements_linux.sh \
-         ./install/install.sh \
-         ./docker/docker-entrypoint.sh
-
-chmod +x ./requirements_linux.sh \
-         ./cloneall \
-         ./install/install.sh \
-         ./build \
-         ./docker/docker-entrypoint.sh
-
-echo ">>>> [4/7] GLOBAL PYTHON REQUIREMENTS"
+echo ">>>> [3/6] PREPARE SCRIPTS & CONFIG"
 ./requirements_linux.sh
-python3 -m pip install --no-cache-dir build
-python3 -m pip install --no-cache-dir -r ./install/python_requirements_lx.txt
+if [ "$VARIANT" = "BIOS" ]; then
+    EXTRA_PIP_FLAGS="--break-system-packages --proxy $PROXY_SERVER"
+    CURRENT_INSTALL_FLAGS="--use-cntlm --docker"
+    BIN_DIR="/usr/bin"
+    DEVTOOLS_DIR="./devtools/."
+    PY_DIR="/usr/lib/python3.12"
+    sed -i 's|destDir=$(realpath $mypath/../..)|destDir=$(realpath $mypath/..)|g' ./install/install.sh
+    export http_proxy="$PROXY_SERVER"
+    export https_proxy="$PROXY_SERVER"
+    export no_proxy="localhost,127.0.0.1,.bosch.com"
 
-echo ">>>> [5/7] BUILD"
-mkdir -p /usr/local/lib/plantuml
-curl -L https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar -o /usr/local/lib/plantuml/plantuml.jar
-mkdir -p /opt/rfwaio/robotvscode/data/extensions/jebbs.plantuml-2.18.1/
-ln -sf /usr/local/lib/plantuml/plantuml.jar /opt/rfwaio/robotvscode/data/extensions/jebbs.plantuml-2.18.1/plantuml.jar
+    CURL_OPTS="$CURL_OPTS -x $PROXY_SERVER"
+else
+    EXTRA_PIP_FLAGS=""
+    CURRENT_INSTALL_FLAGS="--android"
+    BIN_DIR="/usr/local/bin"
+    DEVTOOLS_DIR="../devtools/."
+    PY_DIR="/usr/local/lib/python3.13"
+    export PYBIN=$(which python3)
+    PY_PREFIX=$(dirname $(dirname $PYBIN))
+
+    sed -i "s|../python3lx|$PY_PREFIX|g" build
+    sed -i "s|\${PYDIR}/bin/python3|$PYBIN|g" build
+    sed -i "s|$RF_OPT_DIR/python3|$PY_PREFIX|g" ./config/build/dpkg_build/robot
+
+    echo "Dependency Installation"
+    python3 -m pip install $EXTRA_PIP_FLAGS --no-cache-dir build
+    python3 -m pip install $EXTRA_PIP_FLAGS --no-cache-dir -r ./install/python_requirements_lx.txt
+fi
+
+echo ">>>> [4/6] BUILD"
+PLANTUML_PATH="/usr/local/lib/plantuml/plantuml.jar"
+mkdir -p "$(dirname "$PLANTUML_PATH")"
+curl -sL $CURL_OPTS "https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar" -o "$PLANTUML_PATH"
+
+PLANTUML_EXT_DIR="$RF_OPT_DIR/robotvscode/data/extensions/jebbs.plantuml-2.18.1"
+mkdir -p "$PLANTUML_EXT_DIR"
+ln -sf "$PLANTUML_PATH" "$PLANTUML_EXT_DIR/plantuml.jar"
 
 ./cloneall --config-file="$REPO_CONFIG"
-./install/install.sh --android
+./install/install.sh $CURRENT_INSTALL_FLAGS
 ./build --config-file=$REPO_CONFIG --sub-version=$SubVersion
 
-echo ">>>> [6/7] SYSTEM INSTALLATION & PERMISSIONS"
-mkdir -p /opt/rfwaio/python3/bin /opt/rfwaio/tools /opt/rfwaio/linux/icon /opt/rfwaio/tutorial /opt/rfwaio/documentation /opt/rfwaio/devtools
+echo ">>>> [5/6] SYSTEM INSTALLATION & PERMISSIONS"
+mkdir -p $RF_OPT_DIR/{python3/bin,tools,linux/icon,tutorial,documentation,devtools}
 
-# Python Path Compatibility
-ln -s /usr/local/bin/python3 /opt/rfwaio/python3/bin/python3
-
-# Executables & Assets
-cp ./docker/docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
-chmod +x /usr/local/bin/docker-entrypoint.sh
-cp ./config/build/dpkg_build/robot /usr/local/bin/robot
-chmod +x /usr/local/bin/robot
-cp ./config/build/dpkg_build/postinst.sh /usr/local/bin/initRobotFrameworkAIO.sh
-chmod +x /usr/local/bin/initRobotFrameworkAIO.sh
+echo "Executables & Assets"
+cp ./docker/docker-entrypoint.sh $BIN_DIR/docker-entrypoint.sh
+chmod +x $BIN_DIR/docker-entrypoint.sh
+cp ./config/build/dpkg_build/robot $BIN_DIR/robot
+chmod +x $BIN_DIR/robot
+cp ./config/build/dpkg_build/postinst.sh $BIN_DIR/initRobotFrameworkAIO.sh
+chmod +x $BIN_DIR/initRobotFrameworkAIO.sh
 cp ./config/build/dpkg_build/robot.ico /opt/rfwaio/linux/icon/
 cp ./config/build/dpkg_build/set_robotenv.sh /opt/rfwaio/linux/
 cp ./config/tools/appium.sh /opt/rfwaio/devtools/appium
 chmod +x /opt/rfwaio/devtools/appium
 chmod +x /opt/rfwaio/linux/set_robotenv.sh
 
-# Training & Docs
+echo "Training & Docs"
 cp -R -a ../robotframework-tutorial/[0-9][0-9][0-9]_* /opt/rfwaio/tutorial/
 cp ../robotframework-documentation/book/RobotFrameworkAIO_Reference*${SubVersion}.pdf /opt/rfwaio/documentation/ 2>/dev/null || true
-cp -R -a ../devtools/. /opt/rfwaio/devtools/
+cp -R -a $DEVTOOLS_DIR /opt/rfwaio/devtools/
 cp -R -a ./test/aio-analyzer /opt/rfwaio/tools/aio-analyzer
 cp ./version.txt /opt/rfwaio/
 
-# Apply Permissions
-chown -R root:robot-aio /opt/rfwaio
-chmod -R 0775 /opt/rfwaio
-[ -f /opt/rfwaio/devtools/Appium-Inspector.AppImage ] && chmod 4755 /opt/rfwaio/devtools/Appium-Inspector.AppImage
+echo "Permissions"
+chown -R root:robot-aio $RF_OPT_DIR
+chmod -R 0775 $RF_OPT_DIR
 
-# Workspace folders
-mkdir -p ~/RobotTest/logfiles ~/RobotTest/localconfig ~/RobotTest/testcases ~/RobotTest/tutorial
-cp -R -a /opt/rfwaio/tutorial/. ~/RobotTest/tutorial
-cp -R -a /opt/rfwaio/documentation/. ~/RobotTest/documentation
-chown -R root:robot-aio ~/RobotTest/
-chmod -R 0775 ~/RobotTest/
+echo "Workspace setup"
+mkdir -p ~/RobotTest/{logfiles,localconfig,testcases,tutorial,documentation}
+cp -R -a $RF_OPT_DIR/tutorial/. ~/RobotTest/tutorial/
+cp -R -a $RF_OPT_DIR/documentation/. ~/RobotTest/documentation/
+chown -R root:robot-aio ~/RobotTest
+chmod -R 0775 ~/RobotTest
 
-echo ">>>> [7/7] CLEANING UP BUILD"
-find /usr/local/lib/python3.13 -type d -name "__pycache__" -exec rm -rf {} +
-find /usr/local/lib/python3.13 -name "*.pyc" -delete
-# Remove Appium Inspector
-rm -f /opt/rfwaio/devtools/Appium-Inspector.AppImage
+echo ">>>> [6/6] CLEANING UP BUILD"
+if [ -d "$PY_DIR" ]; then
+    echo "Cleaning up Python cache in $PY_DIR"
+    find "$PY_DIR" -type d -name "__pycache__" -exec rm -rf {} +
+    find "$PY_DIR" -name "*.pyc" -delete
+fi
 
-echo ">>>> Build OSS Finished successfully!"
+rm -f $RF_OPT_DIR/devtools/Appium-Inspector.AppImage
+rm -rf $RF_OPT_DIR/robbotvscode
+
+echo ">>>> Build $VARIANT finished successfully!"
